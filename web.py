18f85986824a5bbef384e79b847d1aa5fa014fc0
Amaya <amayakak3@gmail.com>
2022-12-31 18:12:20 +0530

    Initial Commit

--- web.py
+++ web.py
@@ -20,4 +20,4 @@ for index,todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 st.text_input(label="",placeholder="Add a new todo",on_change=add_todo,key="new_todo")
-st.session_state
+#st.session_state
